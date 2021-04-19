59197a3f694c69505e54031cd0e399aa786c0de6
Kirill Klimenko <klimenkokirillm60@gmail.com>
2021-04-20 02:58:03 +0300

    commit

--- src/commands/utils/CommandReceiver.java
+++ src/commands/utils/CommandReceiver.java
@@ -12,6 +12,8 @@ import javax.xml.transform.TransformerException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CommandReceiver {
     private final CommandInvoker commandInvoker;
@@ -83,45 +85,14 @@ public class CommandReceiver {
     }
 
     public void executeScript(String path) {
-        String line;
-        String command;
-        ArrayList<String> args = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new BufferedInputStream(new FileInputStream(path)), StandardCharsets.UTF_8))) {
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.split(" ")[0].matches("add|update|add_if_max|remove_greater")) {
-                    command = line;
-                    for (int i = 0; i < 8; i++) {
-                        if (line != null) {
-                            line = bufferedReader.readLine();
-                            args.add(line);
-                        } else
-                            System.out.println("Не хватает параметров для создания объекта."); break;
-                    }
-                    Person person = PersonCreator.createPersonInScript(args);
-                    switch (command.split(" ")[0]) {
-                        case "add":
-                            CollectionManager.add(person);
-                            break;
-                        case "update":
-                            CollectionManager.update(person, Integer.parseInt(command.split(" ")[1]));
-                            break;
-                        case "remove_greater":
-                            CollectionManager.removeGreater(person);
-                            break;
-                        case "remove_lower":
-                            CollectionManager.removeLower(person);
-                            break;
-                        case "add_if_max":
-                            CollectionManager.addIfMax(person);
-                            break;
-                    }
-                } else
-                    if (line.split(" ")[0].equals("execute_script") && line.split(" ")[1].equals(ExecuteScriptCommand.getPath()))
-                        System.out.println("Пресечена попытка рекурсивного вызова скрипта.");
-                    else commandInvoker.executeCommand(line.split(" "));
+        try(Scanner scanner = new Scanner("C:\\Users\\Кирилл\\IdeaProjects\\Lab5\\src\\test\\script")) {
+            String line
+            while (scanner.hasNextLine()) {
+                scriptCommands.add(scanner.nextLine());
             }
-        } catch (IOException e) {
-            System.out.println("Ошибка! " + e.getMessage());
+            for (int i = 0; i < scriptCommands.size(); i++) {
+                commandInvoker.executeCommand();
+            }
         }
     }
 
